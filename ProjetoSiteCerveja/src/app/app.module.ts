f30699a4cc9f07a9ec462d30d62e96d0647fc93e
Fabio Kawasaki <binhu_kawa@hotmail.com>
2020-11-21 00:49:22 -0300

    Projeto Site Cerveja

--- ProjetoSiteCerveja/src/app/app.module.ts
+++ ProjetoSiteCerveja/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TelaLoginComponent } from './tela-login/tela-login.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { CervejasComponent } from './cervejas/cervejas.component';
+import { SugestoesComponent } from './sugestoes/sugestoes.component';
+import { EquipeComponent } from './equipe/equipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TelaLoginComponent
+    TelaLoginComponent,
+    SobreComponent,
+    CervejasComponent,
+    SugestoesComponent,
+    EquipeComponent
   ],
   imports: [
     BrowserModule
